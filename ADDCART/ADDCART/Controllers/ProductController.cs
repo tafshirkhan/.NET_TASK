d9b8ace2e3770c04529051180faadf9ccb425766
tafshirkhan <78077360+tafshirkhan@users.noreply.github.com>
2022-02-14 23:11:24 +0600

    ADD CART

---
+++ ADDCART/ADDCART/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using ADDCART.Models.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ADDCART.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            DemoDBEntities db = new DemoDBEntities();
+            var data = db.Products.ToList();
+
+            return View(data);
+        }
+
+        public ActionResult AddCart(int productId)
+        {
+            DemoDBEntities db = new DemoDBEntities();
+            var cart = new List<Product>();
+            var product = db.Products.Find(productId);
+            cart.Add(product);
+            Session["cart"] = cart;
+            return View();
+        }
+    }
+}
